bbf2203300a9501eb8a552e84feef1af9000bc8e
koz1tsk1y <koz1tsk1y@mail.ru>
2024-09-19 11:28:38 +0300

    Изменить верстку главной страницы

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 import '../scss/style.scss'
+
+import { swiper } from './vendors/swiper-bundle.min.js'; // Swiper slider
+import { swiperInit } from './_swiper-init.js'; // Swiper init
+import { masonry } from './vendors/masonry.pkgd.min.js'; // Swiper slider
+
+document.addEventListener('DOMContentLoaded', function() {
+  swiper();
+  swiperInit();
+  masonry();
+})
