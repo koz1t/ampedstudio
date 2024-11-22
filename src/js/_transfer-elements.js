bdb70509da3c70164f8ad0cf42569c8bcc871730
koz1tsk1y <boss.sirovAF@mail.ru>
2024-11-22 17:34:51 +0300

    Сделать вторую версию шапки (кривую, как на my.ampedstudio.com)

--- src/js/_transfer-elements.js
+++ src/js/_transfer-elements.js
@@ -14,12 +14,25 @@ export function transferElements() {
     });
   });
   
-  let element = document.querySelector('.header__linguise');
+  let element = document.querySelector('.header:not(.header--v2) .header__linguise');
   if (element) {
     new TransferElements({
       sourceElement: element,
       breakpoints: {
         900: {
+          targetElement: document.querySelector('.header__burger-menu'),
+          targetPosition: 2,
+        }
+      }
+    });
+  }
+
+  element = document.querySelector('.header--v2 .header__linguise');
+  if (element) {
+    new TransferElements({
+      sourceElement: element,
+      breakpoints: {
+        1279: {
           targetElement: document.querySelector('.header__burger-menu'),
           targetPosition: 2,
         }
